ad3b961f8f7b0c5cd7fdd701a515d0bf7fb2bd44
AdrianTayeh <81419977+AdrianTayeh@users.noreply.github.com>
2025-03-26 14:27:17 +0100

    alriks feature

--- src/js/Message.js
+++ src/js/Message.js
@@ -1,25 +1,20 @@
 export class Message {
-    constructor(username, message, color, shadowBanned=false) {
-        this._username = username;
-        this._message = message;
-        this._color = color;
-<<<<<<< HEAD
-        this._shadowBanned = shadowBanned;
-    }
-    get username() {
-        return this._username;
-    }
-    get message() {
-        return this._message;
-    }
-    get color() {
-        return this._color;
-    } 
-    get shadowBanned() {
-        return this._shadowBanned;
-    } 
+  constructor(username, message, color, shadowBanned = false) {
+    this._username = username;
+    this._message = message;
+    this._color = color;
+    this._shadowBanned = shadowBanned;
+  }
+  get username() {
+    return this._username;
+  }
+  get message() {
+    return this._message;
+  }
+  get color() {
+    return this._color;
+  }
+  get shadowBanned() {
+    return this._shadowBanned;
+  }
 }
-=======
-
-}}
->>>>>>> origin/main
